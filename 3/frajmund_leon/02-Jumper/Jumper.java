9f9629ad0a6427c03ee867919b274921ba1ce39a
voidyknight <leonfrajmund@gmail.com>
2014-02-25 19:16:09 -0500

    Added comment

--- 3/frajmund_leon/02-Jumper/Jumper.java
+++ 3/frajmund_leon/02-Jumper/Jumper.java
@@ -13,6 +13,10 @@ public class Jumper extends Bug{
     public void act(){
 	target = newTarget();
 	if (get(target) != null && isValid(target)){
+	    //I cannot for the life of me get isValid to work, even if it is
+	    //a Grid function and I have Grid imported
+	    //Apparently nobody else that I asked knew how to do it too so I am
+	    //stumped
 	    moveTo(target);
 	}else
 	    turn();
