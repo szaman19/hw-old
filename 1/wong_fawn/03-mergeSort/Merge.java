b919f90647356d7ef7692fa7531296d6037f344f
fawnwong <fawn.wong@live.com>
2014-02-27 19:34:14 -0500

    pushing from school computer

---
+++ 1/wong_fawn/03-mergeSort/Merge.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Merge {
+    public int[] merge(int[] A, int[] B) {
+	int a = 0; b = 0, c = 0;
+	int len = A.length + B.length;
+	int[] C = int[len];
+	for (int i = 0; i < len; i++) {
+	    if (a == A.length || b == B.length) {
+		
+	    }
+	    else if (A[a] > B[b]) {
+		C[i] = A[a];
+		a = a + 1;
+	    }
+	    else {
+		C[i] = B[b];
+		b = b + 1;
+	    }
+	}
+	return C;
+    }      
+}      
