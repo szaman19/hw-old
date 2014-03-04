fd96893c7d6f2152199d22a4a84b375d39de388d
George Drimba <gdrimba@gmail.com>
2014-03-03 20:48:37 -0500

    merge

--- 3/drimba_george/03-Merge/MergeSort.java
+++ 3/drimba_george/03-Merge/MergeSort.java
@@ -1,5 +1,5 @@
 public class MergeSort{
-    public static  int[] merge(int[] a, int[] b){
+    public int[] merge(int[] a, int[] b){
 	int alen = a.length;
 	int blen = b.length;
 	int[] ans = new int[alen + blen];
@@ -24,11 +24,28 @@ public class MergeSort{
 		    ans[i] = a[spota];
 		    spota++;
 		}
-	    }
-
 	}
-	return ans;
+
+    public int[] mergeSort(int[] L){
+	if(L.length <= 1){
+	    return L;
+	}
+	int a = L.length / 2;
+	int b = L.length - a;
+	int[] A = new int[a];int[] B = new int[b];
+	for(int i=0;i<a;i++){
+	    A[i] = L[i];
+}
+	for(int i=0;i<b;i++){
+	    B[i] = L[a+i];
+	}
+	int[] nA = mergeSort(A);
+	int[] nB = mergeSort(B);
+	return merge(nA,nB);
     }
+}
+	return ans;
+}
     public static void main(String[] args){
 	int[] a = new int[]{2,4,8,16,32,64,128,256,512};
 	int[] b = new int[]{3,9,27,81,243,729,2187,2188,2189,2190};
