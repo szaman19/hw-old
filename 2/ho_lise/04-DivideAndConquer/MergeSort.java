92d6e355d27a345f17d58099867f9bde9aef3e98
Lise <novack@ubuntu.(none)>
2014-03-04 22:49:32 -0500

    added description of search algorithm

--- 2/ho_lise/04-DivideAndConquer/MergeSort.java
+++ 2/ho_lise/04-DivideAndConquer/MergeSort.java
@@ -6,32 +6,26 @@ public class MergeSort{
 	int b = 0;// index of ArrayList<String> B
 
 	int c = 0; // index of completed array
-	ArrayList<String> C = new ArrayList<String>(A.size() + B.size());
-	int i = 0;
+	ArrayList<String> C = new ArrayList<String>();
+
 	while (a <A.size() && b <B.size()){
-	    if (A.get(a).charAt(i) < B.get(b).charAt(i)){
-		C.set(c,A.get(a));
+	    if (A.get(a).compareTo(B.get(b))<0){
+		C.add(A.get(a));
 		a++;
-		i=0;
-	    }
-	    else if(A.get(a).charAt(i) == B.get(b).charAt(i) && i<A.size() && i<B.size() ){
-		i++;	
 	    }
 	    else{
-		C.set(c,B.get(b));
+		C.add(B.get(b));
 		b++;
-		i=0;
 	    }
-	    c++;
-	    
+	    c++; 
 	}
 	while (a<A.size()){
-	    C.set(c,A.get(a));
+	    C.add(A.get(a));
 	    c++;
 	    a++;
 	}
 	while (b<B.size()){
-	    C.set(c,B.get(b));
+	    C.add(B.get(b));
 	    b++;
 	    a++;
 	}
@@ -39,13 +33,12 @@ public class MergeSort{
     }
 
     public static ArrayList<String> msort(ArrayList<String> L){
-	if (L.size()<=1){
+	if (L.size() <= 1){
 	    return L;
 	}
 	else{
 	    ArrayList<String> a = new ArrayList<String>(L.subList(0, L.size()/2));
-	    ArrayList<String> b = new ArrayList(L.subList(L.size()/2, L.size()));
-	    System.out.println("*");	    
+	    ArrayList<String> b = new ArrayList<String>(L.subList(L.size()/2, L.size()));
 	    ArrayList<String> A = msort(a);
 	    ArrayList<String> B = msort(b);
 	    L= merge(A,B);
@@ -56,15 +49,15 @@ public class MergeSort{
      
     public static void main (String[]args){
 	MergeSort X = new MergeSort();
-	ArrayList<String> Y = new ArrayList<String>(5);
+	ArrayList<String> Y = new ArrayList<String>();
 	Y.add("Apples");
 	Y.add("Asdf");
 	Y.add("Zsdf");
 	Y.add("Bsdf");
 	Y.add("bsdf");
-	
+
 	System.out.println(Y.toString());
-	X.msort(Y);
-	System.out.println(Y.toString());
+	ArrayList<String> ans = X.msort(Y);
+	System.out.println(ans.toString());
     }
 }
