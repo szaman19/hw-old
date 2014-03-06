9f2c0c93929c23c2586fd2b79488318550bb1d79
omarhegazy <omarbicboi@gmail.com>
2014-03-06 09:28:36 -0500

    english words

--- 2/hegazy_omar/05-MergeComparable/SortDriver.java
+++ 2/hegazy_omar/05-MergeComparable/SortDriver.java
@@ -1,5 +1,5 @@
 /* Only tests Strings and ints */
-
+import java.io.*;
 import java.util.*;
 public class SortDriver {
     public static void main(String[] args) {
@@ -18,6 +18,31 @@ public class SortDriver {
 		}
 		return array;
     }
+	public static ArrayList<String> genRationalStrings(int arraySize)  {
+    	System.out.println("Gettin' your random words...");
+    	try {
+    		int index = 0;
+    		String nextWord;
+    		ArrayList<String> dict = new ArrayList<String>();
+    		Scanner wordReader = new Scanner(new FileReader("/usr/share/dict/words")); //better be runnin' unix
+    		//no upper-case words because "StupidASCIIsort" comes before "idiotASCIIsort" cause ASCII sort was apparently designed to be counterintuitive
+    		while (wordReader.hasNext()) 
+				if ((int) (nextWord = wordReader.nextLine()).charAt(0) >= 97) 
+					dict.add(nextWord);
+			ArrayList<String> answer = new ArrayList<String>();
+			for (int i = 0; i < arraySize; i++) 
+				answer.add(dict.get((int) (Math.random() * dict.size())));
+			
+			return answer;
+    	} catch (IOException e) {
+    		System.out.println("I'm tryna get /usr/share/dict/words. If you're not running Unix, you don't have it, this function won't work");
+    		System.out.println("I could make the function take a file path parameter and let you get access to that from String[] args...");
+    		System.out.println("But you could be running a Unix-like system.");
+    		System.out.println("As punishment, you get random, meaningless strings :");
+    		return genStrings(arraySize,6);
+    	}
+    }
+	
 	public static ArrayList<Integer> genInt(int size) {
     	ArrayList<Integer> array = new ArrayList<Integer>();
     	for (int i = 0; i < size; i++) {
@@ -31,7 +56,7 @@ public class SortDriver {
 	public static void mergeStringTest() {
 		ArrayList<String> array = new ArrayList<String>();
 		System.out.println("Merge Test (String):");
-		array = genStrings(100,100);
+		array = genRationalStrings(40);
 		System.out.println(array);
 		double time = System.currentTimeMillis();
 		array = Sort.merge(array);
@@ -44,7 +69,7 @@ public class SortDriver {
 	public static void mergeDoubleTest() {
 		ArrayList<Integer> array = new ArrayList<Integer>();
 		System.out.println("Merge Test:");
-		array = genInt(3000);
+		array = genInt(40);
 		System.out.println(array);
 		double time = System.currentTimeMillis();
 		array = Sort.merge(array);
