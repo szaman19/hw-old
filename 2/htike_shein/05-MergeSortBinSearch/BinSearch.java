6ac96936daa8c0617b0cd6f3776eb3a27ec743ae
Shein Htike <chocolateshein.htike@gmail.com>
2014-03-06 16:10:35 -0500

    Binary Search

---
+++ 2/htike_shein/05-MergeSortBinSearch/BinSearch.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+public class BinSearch{
+	public static int rbsearch(int n,int[] l){
+		int h = l.length / 2;
+		if(l.length == 1 && n != l[h]){
+			return -1;
+		}
+		else if(n == l[h]){
+			return h;
+		}
+		else if(n > l[h]){
+			return h + 1 + rbsearch(n,Arrays.copyOfRange(l,h + 1,l.length));
+		}
+		else{
+			return rbsearch(n,Arrays.copyOfRange(l,0,h));
+		}
+	}
+	public static int ibsearch(int n,int[] l){
+		int ub = l.length;
+		int lb = 0;
+		int h = lb + (lb + ub) / 2;
+		while(n != l[h]){
+			if(ub - lb == 1)
+				return -1;
+			else{
+				if(n > l[h])
+					lb = h + 1;
+				else
+					ub = h;
+			}
+			h = lb + (lb + ub) / 2;
+		}
+		return h;
+	}
+	public static void main(String[] args){
+		int[] args2 = new int[args.length - 1];
+		int f = Integer.parseInt(args[0]);
+		for(int i = 1;i < args.length;i++){
+			args2[i - 1] = Integer.parseInt(args[i]);
+		}
+		System.out.println(rbsearch(f,args2));
+		System.out.print(ibsearch(f,args2));
+	}
+}
