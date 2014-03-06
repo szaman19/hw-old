4d08168a2159f322258e03a7d062a43d6e3a5092
Youbin Kim <nibuoy1212@gmail.com>
2014-03-06 18:53:13 -0500

    updating hw4

--- 3/kim_youbin/04-DivConquer/Merge.java
+++ 3/kim_youbin/04-DivConquer/Merge.java
@@ -1,85 +1,82 @@
+import java.util.*;
+
 public class Merge{
-    int[] start;
+    ArrayList<String> start;
     
-    public Merge(int[] a){
+    public Merge(ArrayList<String> a){
 	start = a;
     }
 
     
-    public int[] mergesort(int[] a){
-	if(a.length == 1){
+    public ArrayList<String> mergesort(ArrayList<String> a){
+	if(a.size() == 1){
 	    return a;
 	}else{
 	    
-	    int[] x = new int[(int)Math.ceil(a.length/2.0)];
-	    int[] y = new int[(int)Math.floor(a.length/2.0)];
+	    ArrayList<String> x = new ArrayList<String>();
+	    ArrayList<String> y = new ArrayList<String>();
 	    
-
 	    int count = 0;
-
-	    for (int i = 0; i < x.length; i++){
-		x[i] = a[count];
+	    
+	    for (int i = 0; i < a.size()/2; i++){
+		x.add(a.get(count));
 		count++;
 	    }
-	    for (int i = 0; i < y.length; i++){
-		y[i] = a[count];
+	    for (int i = 0; i < (a.size()-x.size()); i++){
+		y.add(a.get(count));
 		count++;
 	    }
-
+	    
 	    //	    System.out.println(pt(a) + "-> " + pt(x) + "+ " + pt(y));
-
-	    int[] x1 = mergesort(x);
-	    int[] y1 = mergesort(y);
-
+	
+	    ArrayList<String> x1 = mergesort(x);
+	    ArrayList<String> y1 = mergesort(y);
+    
 	    return merge(x1,y1);
 	}
     }
     
-    public int[] merge(int[] a, int[] b){
-	int x = 0;
-	int y = 0;
-	int [] ans = new int[a.length + b.length];
-	
-	for (int i = 0; i< ans.length; i++){
-	    if (x == a.length){
-		ans[i] = b[y];
-		y++;
-	    } else if (y == b.length){
-		ans[i] = a[x];
-		x++;
-	    } else if (a[x] < b[y]){
-		ans[i] = a[x];
-		x++;
-	    } else {
-		ans[i] = b[y];
-		y++;
+    public ArrayList<String> merge(ArrayList<String> a, ArrayList<String> b){
+	ArrayList<String> ans = new ArrayList<String>();
+	System.out.print(pt(a) + "+ " + pt(b) + "= ");
+	while (a.size() > 0 && b.size() > 0){
+	    if (a.get(0).compareToIgnoreCase(b.get(0))<0){
+		ans.add(a.get(0));
+		a.remove(0);
+	    }else {
+		ans.add(b.get(0));
+		b.remove(0);
 	    }
-	    
 	}
-	//	System.out.println(pt(a) + "+ " + pt(b) + "= " + pt(ans));
+	if (a.size() == 0){
+	    ans.addAll(b);
+	}else{
+	    ans.addAll(a);
+	}
+	
+     	//      	System.out.println(pt(ans));
 	return ans;
     }
-
-    public String pt(int[] a){
+   
+    public String pt(ArrayList<String> a){
 	String ans = "";
-	for (int i = 0; i < a.length; i++){
-	    ans += (a[i] + " ");
+	for (int i = 0; i < a.size(); i++){
+	    ans += (a.get(i) + " ");
 	}
 	return ans;
     }
     
     public static void main(String[] args){
-	int num = Integer.parseInt(args[0]);
-	int r = Integer.parseInt(args[1]);
-	
-	int[] arr = new int[num];
-	
-	for (int i = 0; i < num; i++){
-	    arr[i] = (int)(r * Math.random());
-	}
-	
+	ArrayList<String> arr = new ArrayList<String>();
+	arr.add("adga");
+	arr.add("wefa");
+	arr.add("njdsfd");
+	arr.add("elkjdsg");
+	arr.add("ehksa");
 	Merge n = new Merge(arr);
 	System.out.println(n.pt(n.mergesort(n.start)));
-    }
+	//System.out.println(n.pt(arr));
+
+ }
 
 }
