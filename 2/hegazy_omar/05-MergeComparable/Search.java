772c10653e7e377d338c3699abe34dba48c47f74
omarhegazy <omarbicboi@gmail.com>
2014-03-06 11:30:08 -0500

    binary search

--- 2/hegazy_omar/05-MergeComparable/Search.java
+++ 2/hegazy_omar/05-MergeComparable/Search.java
@@ -1,13 +1,27 @@
 import java.util.*;
 public class Search {
 	public static void main(String[] args) {	
-		int[] test = {1,2,3,4,5,6,7,8};
-		System.out.println(rbsearch(4,test));
 	}
 	public static int rbsearch(int item, int[] sortedArray) {
-		Arrays.sort(sortedArray); //sort all arrays w/ quicksort takes less time than check if its already sorted, then sort it accordingly
+		Arrays.sort(sortedArray); //sorting all arrays w/ quicksort takes less time than check if its already sorted, then sort it accordingly
 		return rbsearch(item, 0, sortedArray.length - 1, sortedArray);
 
+	}
+	public static int ibsearch(int item, int[] array) {
+		Arrays.sort(array);
+		int lowest, highest, middle;
+		lowest = 0;
+		highest = array.length - 1;
+		while (highest >= lowest) {
+			middle = (int) ((lowest + highest) / 2);
+			if (array[middle] == item)
+				return middle;
+			if (item > array[middle])
+				lowest = middle + 1;
+			if (item < array[middle])
+				highest = middle - 1;
+		}
+			return -1;
 	}
 	private static int rbsearch(int item, int lowest, int highest, int[] array) {
 		int middle = (int) ((lowest + highest) / 2);
