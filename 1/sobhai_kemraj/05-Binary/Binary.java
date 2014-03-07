700fe9630736bdc5560a5cf63eed584f4c0a670b
Kemraj Sobhai <kbsobhai@gmail.com>
2014-03-07 08:42:15 -0500

    needs work

--- 1/sobhai_kemraj/05-Binary/Binary.java
+++ 1/sobhai_kemraj/05-Binary/Binary.java
@@ -2,23 +2,23 @@ import java.util.*;
 import java.io.*;
 
 public class Binary{
-    public int[] binarySearch(int n, int[] L){
+    public int binarySearch(int n, int[] L){
 	if (L[(L.length/2)] == n)
 	    return L.length/2;
 	else{
 	    if (n < L.length/2)
-		return binarySearch(n, Arrays.copyOfRange(L, 0, (L.length/2)-1));
+		return binarySearch(n, Arrays.copyOfRange(L, 0, (L.length/2)));
 	    else
 		return binarySearch(n, Arrays.copyOfRange(L, (L.length/2), L.length-1));
 	}
     }
 
-    public int[] iterativeBinarySearch(int n, int[] L){
+    public int iterativeBinarySearch(int n, int[] L){
 	boolean work = true;
 	while (work){
-	    if (L.length == 1 || L.length == 0)
+	    if (L.length <= 1 && L[0] != n)
 		work = false;
-	    if ((L[L.length/2])==n)
+	    else if ((L[L.length/2])==n)
 		return L.length/2;
 	    else{
 		if (n<L.length/2){
@@ -30,5 +30,15 @@ public class Binary{
 	    }
 	}
 	return -1;
+    }
+
+    public static void main (String[] args){
+	Binary b = new Binary();
+	
+	int[] a = new int[512];
+	for (int i = 0; i < a.length; i ++)
+	    a[i] = i*4;
+	
+	System.out.println(b.iterativeBinarySearch(4, a));
     }
 }
