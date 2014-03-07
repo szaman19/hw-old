e8e96d505de175c238fe537fd6dc727883186903
nicholasromanoff <nicholas.romanoff@aol.com>
2014-03-07 01:01:00 -0500

    HW05 Finished

--- 1/romanoff_nicholas/05 MergeSort BinSearch/BinarySearch.java
+++ 1/romanoff_nicholas/05 MergeSort BinSearch/BinarySearch.java
@@ -6,40 +6,50 @@
 import java.util.*;
 
 public class BinarySearch {
-/*
-target 33
-0  4 5 7  9 20 33 35
-middle 4 
-guess 9
-low 0
-high 7
-*/
 
 	// conducts an iterative binary search
 	public static int iterativeBinarySearch(int target, int[] list) {
-        	int targetIndex = -1; 
 		int low = 0;
-		int high = list.length;
-        	int middle = (low + high) / 2; 
+		int high = list.length - 1;
         	while (low <= high) { 
-            		middle = (low + high) / 2;
+            		int middle = (low + high) / 2;
             		if (list[middle] == target) {
-				targetIndex = middle;
-				break;
+				return middle;
+			}
+            		else if (list[middle] < target) {
+				low = middle + 1;
 			}
             		else if (list[middle] > target) {
 				high = middle - 1;
 			}
-                
-            		else if (list[middle] < target) {
-				low = middle + 1;
-			}
 		}
-		return middle;
+		return -1;
 	}
 
+	// helper function - calls a recursive binary search
+    	public static int recursiveBinarySearch(int target, int[] array) {
+        	return recursiveHelper(array, target, 0, array.length - 1);
+	}
 
-	public void print(int[] a) { 
+	// conducts a recursive binary search
+   	public static int recursiveHelper(int[] array, int target, int low, int high) {
+        	int midpoint = (low + high) / 2;
+        	if (low > high) { 
+			return -1;
+		}
+        	if (target == array[midpoint]) { 
+			return midpoint;
+		}
+        	else if (array[midpoint] > target) {
+			return recursiveHelper(array, target, low, midpoint - 1);	
+		}		
+
+        	else {
+			return recursiveHelper(array, target, midpoint + 1, high);
+		}		
+
+	}
+	public static void printIntArray(int[] a) { 
 		String printStr = "[ ";
         	for (int n : a) {
 			printStr += ((Integer) n) + ", ";
@@ -47,24 +57,11 @@ high 7
 		printStr = printStr.substring(0, printStr.length() - 2) + " ]";
 		System.out.println(printStr);
 	}
-
-
 	public static void main(String[] args) {
-		BinarySearch bs = new BinarySearch();
-		int[] a = new int[20];
-        	for (int x = 0; x < 20; x++) {
-			a[x] = (int)(20 * Math.random());
-        	}
-		Arrays.sort(a);
-		bs.print(a);
-		for (int x = 0; x < 5; x++) {
-			int p = (int)(20 * Math.random());
-
-		}			
+		BinarySearch rb = new BinarySearch();
+		int[] a = {1, 2, 16, 17, 20, 21, 27, 28, 30};
+		System.out.println(rb.recursiveBinarySearch(27, a));
 	}
-
-
-
 
 
 
