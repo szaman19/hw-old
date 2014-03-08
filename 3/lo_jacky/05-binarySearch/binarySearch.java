82681521dd991521726bc8052ce7d56cced21bc0
Jacky Lo <jljcky@aim.com>
2014-03-07 21:16:13 -0800

    added HW5

---
+++ 3/lo_jacky/05-binarySearch/binarySearch.java
@@ -0,0 +1,15 @@
+public class binarySearch{
+    /*start in the middle
+     *see if the number is in the first or second half
+     *if not return -1
+     *else return index
+     *much confuse*/
+
+    public int rbsearch(int n, String[] L){
+	return -1;
+    }
+
+    public int ibsearch(int n, int[] L){
+	return -1;
+    }
+}
