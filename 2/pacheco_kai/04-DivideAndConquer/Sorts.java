1d645ad2cadab86b5b9b72838f63954cc70ce39b
kaip629 <kaip629@gmail.com>
2014-03-09 11:22:37 -0400

    small changes

--- 2/pacheco_kai/04-DivideAndConquer/Sorts.java
+++ 2/pacheco_kai/04-DivideAndConquer/Sorts.java
@@ -1,18 +1,18 @@
 public class Sorts{
     public static ArrayList<String> mergeSort(ArrayList<String> a){
-    if(a.length<=1){
+    if(a.size<=1){
       return a;
     }else{
 	System.out.println(toString(a));
 	ArrayList<String>h1=new ArrayList<String>;
 
-	for(int x=0;x<a.length/2;x++){
+	for(int x=0;x<a.size/2;x++){
 	    h1[x]=a[x];
 	}
 	System.out.println(toString(h1));
 	ArrayList<String>h2=new ArrayList<String>;
-	for(int x=0;x<(a.length-a.length/2);x++){
-	    h2[x]=a[h1.length+x];
+	for(int x=0;x<(a.size-a.size/2);x++){
+	    h2[x]=a[h1.size+x];
 	}
 	System.out.println(toString(h2));
 	return merge(mergeSort(h1),mergeSort(h2));
@@ -20,27 +20,27 @@ public class Sorts{
     }
     public static ArrayList<String> merge(ArrayList<String> a, ArrayList<String> b){
 	int n;
-	int[] ans=new int[a.length+b.length];
+	int[] ans=new int[a.size+b.size];
 	int asmallest=0;
 	int bsmallest=0;
-	if(b.length>a.length){
-	    n=a.length*2;
+	if(b.size>a.size){
+	    n=a.size*2;
 	}else{
-	    n=b.length*2;
+	    n=b.size*2;
 	}
 	for(int x=0;x<n;x++){
 	    if(a[asmallest]<=b[bsmallest]){
 		ans[x]=a[asmallest];
-		if(asmallest<a.length-1){
+		if(asmallest<a.size-1){
 		    asmallest++;}
 	    }else{
 		ans[x]=b[bsmallest];
-		if(bsmallest<b.length-1){
+		if(bsmallest<b.size-1){
 		    bsmallest++;}
 	    }
 
 	}
-	while(n<ans.length){
+	while(n<ans.size){
 	    if(b[bsmallest]>a[asmallest]){
 		ans[n]=b[bsmallest];
 		n++;
@@ -57,21 +57,21 @@ public class Sorts{
     
     public static String toString(int[] a){
 	String ans="";
-	for(int x=0;x<a.length;x++){
+	for(int x=0;x<a.size;x++){
 	    ans+=a[x];
 	}
 	return ans;
     }
     
     public static void main(String[]args){
-	int[] hi = new int[7];
-	hi[0]=1;
-	hi[1]=2;
-	hi[2]=4;
-	hi[3]=6;
-	hi[4]=2;
-	hi[5]=3;
-	hi[6]=7;
+	ArrayList<String> hi = new ArrayList<String>();
+	hi.add("w");
+	hi.add("y");
+	hi.add("c");
+	hi.add("zx");
+	hi.add("za");
+	hi.add("h");
+	hi.add("x");
 	System.out.println(toString(mergeSort(hi)));
 	
     }
