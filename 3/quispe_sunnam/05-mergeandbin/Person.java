fd34e8d332da6161ab0b4a63e02a7d00323b3c85
konceq <sunnamquispe@gmail.com>
2014-03-09 19:09:03 -0400

    added Person

--- 3/quispe_sunnam/05-mergeandbin/Person.java
+++ 3/quispe_sunnam/05-mergeandbin/Person.java
@@ -3,7 +3,7 @@ public class Person implements Comparable {
     private int age;
 
     public Person() {
-	name = new String("Joe Smith");
+	name = new String("Foo Bar");
 	age = 21;
     }
 
@@ -21,17 +21,17 @@ public class Person implements Comparable {
 
     public int compareTo(Object x) {
 	if (x == null) {
-	    throw(new NullPointerExcepton("\n This is null."));
+	    throw(new NullPointerExcepton("\n Null"));
 	}
 	else if (! (x instanceof Person)) {
-	    throw (new ClassCastException("\n This is not a person!"));
+	    throw (new ClassCastException("\n Not a person"));
 	}
 	return (this.getname().compareTo(((Person)x).getname())) + (this.getage() - ((Person)x).getage());
 	    }
 
     public boolean comp(Object y) {
 	if (x == null) {
-	    throw (new NullPointerException("\n This is null."));
+	    throw (new NullPointerException("\n Null"));
 	}
 	return ((x instanceof Person) || ((this.getName().compareTo(((person))x).getname()) == 0) && (this.getage() == (((Person)x).getage())));
     }
