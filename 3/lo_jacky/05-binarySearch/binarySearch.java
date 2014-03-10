24395a68641bfe62f0f167b2329a5267d85578b4
Jacky Lo <jljcky@aim.com>
2014-03-09 21:47:58 -0700

    Edited HW5

--- 3/lo_jacky/05-binarySearch/binarySearch.java
+++ 3/lo_jacky/05-binarySearch/binarySearch.java
@@ -1,38 +1,42 @@
 public class binarySearch{
     //only for sorted lists
 
-
-    public static int rbsearch(int n, int[] L){
-	int avr = L.length/2;
-	if (L.length <= 1)
-	    return -1;
-	if (n == L[avr])
-	    return avr;
-	int[] h1 = new int[avr];
-	for (int i = 0; i < h1.length; i++)
-	    h1[i] = L[i];
-	int[] h2 = new int[L.length - h1.length];
-	for (int i = 0; i < h2.length; i++)
-	    h2[i] = L[i+avr];
-	int a = rbsearch(n, h1);
-	if (a == -1)
-	    a = rbsearch(n, h2);
-	return a;
+    public static int rbsearch(int n, int[] L, int low, int high){
+	if (low <= high && low >= 0 && high < L.length && n >= low && n <= high){
+	    int avr = (low + high) / 2;
+	    if (L[avr] == n)
+		return avr;
+	    else if (L[avr] < n)
+		return rbsearch(n, L, avr, high);
+	    else if (L[avr] > n)
+		return rbsearch(n, L, low, avr);
+	}
+	return -1;
     }
 
     public static int ibsearch(int n, int[] L){
-        return -1;
+	int low = 0;
+	int high = L.length - 1;
+	for (int i = 0; i < L.length; i++){
+	    int avr = (low + high) / 2;
+	    if (L[avr] == n)
+		return avr;
+	    else if (L[avr] < n)
+		low = avr+1;
+	    else
+		high = avr-1;
+	}
+	return -1;
     }
 
     public static void main(String[] args){
 	int[] a = new int[10];
 	for (int i = 0; i < 10; i++)
 	    a[i] = i;
-	mergeSort b = new mergeSort();
-	a = b.mergeSort(a);
-	for (int i = 0; i < 10; i++)
-	    System.out.print(a[i] + ", ");
-	System.out.println();
-	System.out.println(rbsearch(6,a));
+	System.out.println(rbsearch(2, a, 0, 5));
+	System.out.println(rbsearch(11,a, 0, 9));
+	System.out.println(rbsearch(2, a, 5, 9));
+	System.out.println(ibsearch(8, a));
+	System.out.println(ibsearch(10,a));
     }
 }
