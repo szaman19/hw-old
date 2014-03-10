998425ad7280fbd534ffb12c7437c7d20bd1e26f
les-bres <les.bres.2@gmail.com>
2014-03-09 20:29:16 -0400

    add Person.java

---
+++ 2/bresnahan_leslie/05-mergebin/Person.java
@@ -0,0 +1,22 @@
+public class Person implements Comparable {
+
+    private int age;
+    private String name;
+
+    public Person( int ageG, String nameG) {
+	age = ageG;
+	name = nameG;
+    }
+
+    public int getAge() {
+	return age;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public int compareTo( Object other ) {
+	return age - ((Person)other).age;
+    }
+}
