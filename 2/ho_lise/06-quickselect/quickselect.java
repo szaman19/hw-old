66a42ec9e9fc04c49569ff353814bc86286aad8c
Lise <novack@ubuntu.(none)>
2014-03-11 02:57:48 -0400

    added quickselect- needs bug fixing

--- 2/ho_lise/06-quickselect/quickselect.java
+++ 2/ho_lise/06-quickselect/quickselect.java
@@ -1,38 +1,54 @@
 import java.util.*;
 public class quickselect{
     public static int quickselect(int[] a, int k, int low, int high){
+	System.out.println("\n"+low + " " + high);
 	if (low >= high){
 	    return a[k];
 	}
-	int p =a[(int)(Math.random()*(high-low))];
-	int[] F = new int[a.length];
+	int p =a[(int)(Math.random()*(high-low))+low];
+	int[] F = new int[high-low+1];
 	int f=0;
-	int[] E = new int[a.length];
 	int e=0;
-	for (int i =0; i < a.length; i++){
-	    if (a[i] <p){
+	for (int i =low; i <= high; i++){
+	    if (a[i]<p){
 		F[f] = a[i];
 		f++;
 	    }
 	    else if (a[i] > p){
-		E[e] = a[i];
+	System.out.println(Arrays.toString(F) + " " +Arrays.toString(a));
+	System.out.println(high-low-e + "     " + p + "    " + e);
+		F[high-low-e] = a[i];
 		e++;
 	    }
-	}      
-	System.out.println(Arrays.toString(F) + " " + Arrays.toString(E));
-	System.out.println(k + " " + e + " " + f);
+	} 
+	System.out.println(p + " " + e + " " + f);
+
+	System.out.println(Arrays.toString(F) + " " +Arrays.toString(a));
+	for (int g =f; g<=high-e; g++){
+	    System.out.println("***" + g);
+	    F[g] = p;
+	}
+	System.out.println(Arrays.toString(F) + " " +Arrays.toString(a));
+	for (int t = 0; t<=F.length-1;t++){
+	    a[t+low] = F[t];
+	}
+	System.out.println(Arrays.toString(F) + " " +Arrays.toString(a));
+/*
+	for (int x=low; x<=high;x++){
+	    F[x-low] = a[x];
+	    }*/
+
 	// if p is within the bounds between the gap of the front array and the end array compared to the total array length, then p is our solution.
 	//for example [1,2,3,4] [6,7,45], if the array has 8 characters and we are looking for the fifth elements, instead of adding the multiple copies of p into one of the array, we can skip that step and just return p
-	if (a.length-e >=k && f <=k ){
+	if (f+low==k){
 	    return p;
 	}
-       	else if (f > k){
-	    return quickselect(E,k-e,f,high);
+       	else if (f < k){
+	    return quickselect(a,k,f+low+1,high);
 	}
 	else{
-	    return quickselect(F,k-f,low, f);
-	}
-	
+	    return quickselect(a,k,low,f+low-2);
+	}	
     }
 
     public static void main (String[]args){
