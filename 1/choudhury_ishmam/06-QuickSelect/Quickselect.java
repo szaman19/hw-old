cb4c65c298aa3495f0132d7a92b9020b4a77778d
Ishmam Choudhury <i.choudhury9@gmail.com>
2014-03-12 04:21:13 -0400

    quickselect...sorta

---
+++ 1/choudhury_ishmam/06-QuickSelect/Quickselect.java
@@ -0,0 +1,11 @@
+//I got home at 8 and felt a little sick, so I thought taking a 2 hour nap would help.
+//Sadly my body decided to sleep through alarms until 4AM and I have to start getting
+//ready for school in about a half hour. I will work on this later today.
+//Also, hopefully me not quite understanding will be cleared up..
+import java.io.*;
+import java.util.*;
+
+public class Quickselect {
+	
+	public int quickSelect( int[] a, ink k, int low, int high ) {
+		
