cbb0d148d88e938984febfd2189089d7bfeb3015
Ishmam Choudhury <i.choudhury9@gmail.com>
2014-03-12 04:50:24 -0400

    quickselect

--- 1/choudhury_ishmam/06-QuickSelect/Quickselect.java
+++ 1/choudhury_ishmam/06-QuickSelect/Quickselect.java
@@ -1,6 +1,6 @@
 //I got home at 8 and felt a little sick, so I thought taking a 2 hour nap would help.
 //Sadly my body decided to sleep through alarms until 4AM and I have to start getting
-//ready for school in about a half hour. I will work on this later today.
+//ready for school right now. I will work on this later today.
 //Also, hopefully me not quite understanding will be cleared up..
 import java.io.*;
 import java.util.*;
@@ -8,4 +8,8 @@ import java.util.*;
 public class Quickselect {
 	
 	public int quickSelect( int[] a, ink k, int low, int high ) {
+		int piv = high - low / 2;
+		int[] temp = new int[high-low+1];
+		for (int i = 0; i < a.length; i++) {
 		
+		}
