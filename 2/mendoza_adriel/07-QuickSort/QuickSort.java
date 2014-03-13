21f9ee08651bbd03584b0d97bce2654fd58edad9
Adriel Mendoza <adriel.mendoza@cslab1-1.stuy.edu>
2014-03-12 21:34:04 -0400

    Unfinished HW 07

---
+++ 2/mendoza_adriel/07-QuickSort/QuickSort.java
@@ -0,0 +1,23 @@
+/*import java.util.*;
+
+public class QuickSort{
+    public void QuickSort(int[] a, int low, int high){
+	if(a.length <= 1){
+	    return a;
+	}
+	int i = (int)(Math.random() * (low - high)) + low;
+	int pivot = a[i];
+	ArrayList<String> left;
+	ArrayList<String> right;
+	for(int j = 0; j < a.length; j++){
+	    if(pivot > a[j]){
+		left.add(a[j]);
+	    }
+	    if(pivot < a[j]){
+		right.add(a[j]);
+	    }
+	}
+    }
+}*/
+	
+	
